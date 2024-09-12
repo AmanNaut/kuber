0acbee3f421b0fa0fd6efc6e35705c5b57bbc1a7
amannautiyal <aman.nautiyal@nagarro.com>
2024-09-12 23:39:35 +0530

    kuberAssignment

---
+++ KuberAssignment1/src/main/java/com/nagarro/KuberAssignment1/KuberAssignment1Application.java
@@ -0,0 +1,13 @@
+package com.nagarro.KuberAssignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberAssignment1Application.class, args);
+	}
+
+}
---
+++ KuberAssignment1/src/test/java/com/nagarro/KuberAssignment1/KuberAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.KuberAssignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
