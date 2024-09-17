ad94ec8a2d127287117ad070edeba4f69c856b37
AmanNaut <132882991+AmanNaut@users.noreply.github.com>
2024-09-17 16:35:02 +0000

    updated file with ports

--- KuberAssignment1/src/main/java/com/nagarro/KuberAssignment1/controller/HelloWorldController.java
+++ KuberAssignment1/src/main/java/com/nagarro/KuberAssignment1/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.nagarro.KuberAssignment1.controller;
+clearpackage com.nagarro.KuberAssignment1.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
